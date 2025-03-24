8d8b1022420d91a26e55fb82045d2d0560f8a2de
Lun <luntik.s.granatoi777@gmail.com>
2025-03-23 19:52:54 -0700

    lint fixes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -27,7 +27,7 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=100, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-                               validators=[MaxValueValidator(2025), MinValueValidator(2015)])
+        validators=[MaxValueValidator(2025), MinValueValidator(2015)])
 
     def __str__(self):
         return self.name
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,15 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://starovierov0-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'https://starovierov0-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'https://starovierov0-3030.theiadockernext-0-labs-prod \
+    -theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'https://starovierov0-3030.theiadockernext-0-labs-prod \
+    -theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
